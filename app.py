e9d8b2dfa3aa6e69bb996faed609a5e1c7f706b7
Mk7779 <144084136+Mk7779@users.noreply.github.com>
2026-02-04 18:43:38 +0530

    Update text input prompt for user name

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.set_page_config(page_title="My First Streamlit App")
 st.title("🚀 My Streamlit Cloud App")
 st.write("This app is deployed on Streamlit Cloud!")
 
-name = st.text_input("Enter your name")
+name = st.text_input("Enter your name if mk" )
+name = st.text_input("Enter your name if mk" )
 
 if name:
     st.success(f"Hello, {name} 👋")
